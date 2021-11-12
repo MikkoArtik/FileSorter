8693b4d9e96a2fd7ebb251800b83a15430456c80
Michael Chernov <mihail.tchernov@yandex.ru>
2021-11-12 14:45:32 +0300

    Убран класс создания пар гравиметрических файлов

--- sorters/gravic_sorter.py
+++ sorters/gravic_sorter.py
@@ -1,10 +1,14 @@
 from typing import List, Tuple, Union
 from datetime import datetime
+from datetime import timedelta
 import os
 
 
 TSF_EXTENSION = 'tsf'
 DAT_EXTENSION = 'dat'
+
+DAT_FIRST_LINE_INDEX = 21
+TSF_FIRST_LINE_INDEX = 42
 
 
 class TSFile:
@@ -17,12 +21,12 @@ class TSFile:
             raise OSError(f'File is not tsf-file')
 
         self.path = path
-        self.__last_line = self.__get_last_line()
+        self.__first_line, self.__last_line = self.__get_last_lines()
 
-    def __get_last_line(self) -> str:
+    def __get_last_lines(self) -> Tuple[str, str]:
         with open(self.path) as file_ctx:
             lines = [x.rstrip() for x in file_ctx if len(x.rstrip())]
-        return lines[-1]
+        return lines[TSF_FIRST_LINE_INDEX], lines[-1]
 
     def __get_datetime_from_line(self, line: str):
         datetime_src = list(map(int, line.split()[:6]))
@@ -36,6 +40,10 @@ class TSFile:
     def datetime_stop(self) -> datetime:
         return self.__get_datetime_from_line(self.__last_line)
 
+    @property
+    def datetime_start(self) -> datetime:
+        return self.__get_datetime_from_line(self.__first_line)
+
 
 class DATFile:
     def __init__(self, path: str):
@@ -47,12 +55,12 @@ class DATFile:
             raise OSError(f'File is not tsf-file')
 
         self.path = path
-        self.__last_line = self.__get_last_line()
+        self.__first_line, self.__last_line = self.__get_last_lines()
 
-    def __get_last_line(self) -> str:
+    def __get_last_lines(self) -> Tuple[str, str]:
         with open(self.path) as file_ctx:
             lines = [x.rstrip() for x in file_ctx if len(x.rstrip())]
-        return lines[-1]
+        return lines[DAT_FIRST_LINE_INDEX], lines[-1]
 
     def __get_datetime_from_line(self, line: str) -> datetime:
         date_src, time_src = line.split('\t')[1: 3]
@@ -61,6 +69,11 @@ class DATFile:
 
     def __get_station_from_line(self, line: str) -> str:
         return line.split('\t')[0]
+
+    @property
+    def datetime_start(self) -> datetime:
+        line_datetime = self.__get_datetime_from_line(self.__first_line)
+        return line_datetime + timedelta(minutes=-1, seconds=1)
 
     @property
     def datetime_stop(self) -> datetime:
@@ -75,70 +88,3 @@ class DATFile:
         with open(self.path) as f:
             need_line = f.readlines()[2].rstrip()
         return need_line.split('\t')[-1]
-
-
-class PairFinder:
-    def __init__(self, root: str):
-        if not os.path.exists(root):
-            raise OSError
-
-        self.root = root
-
-        self.tsf_files, self.dat_files = self.get_gravimetric_files()
-        self.used_indexes = set()
-        self.dat_tsf_pairs = self.create_pairs()
-
-    def get_gravimetric_files(self) -> Tuple[List[TSFile], List[DATFile]]:
-        tsf_files_list, dat_files_list = [], []
-        for root, _, files in os.walk(self.root):
-            for filename in files:
-                path = os.path.join(root, filename)
-                try:
-                    tsf_file_info = TSFile(path)
-                    tsf_files_list.append(tsf_file_info)
-                except OSError:
-                    try:
-                        dat_file_info = DATFile(path)
-                        dat_files_list.append(dat_file_info)
-                    except OSError:
-                        pass
-        return tsf_files_list, dat_files_list
-
-    def find_dat_pair(self,
-                      tsf_file_index: int) -> Union[int, None]:
-        tsf_file = self.tsf_files[tsf_file_index]
-        dev_num_part = tsf_file.device_num_part
-        for dat_file_index, dat_file in enumerate(self.dat_files):
-            if dat_file_index in self.used_indexes:
-                continue
-
-            dat_dev_num_part = dat_file.device_full_number[-len(dev_num_part):]
-            if dat_dev_num_part != dev_num_part:
-                continue
-
-            if dat_file.datetime_stop != tsf_file.datetime_stop:
-                continue
-            return dat_file_index
-        else:
-            return
-
-    def create_pairs(self) -> List[Tuple[int, int]]:
-        pairs = []
-        self.used_indexes = set()
-        for tsf_file_index in range(len(self.tsf_files)):
-            dat_index = self.find_dat_pair(tsf_file_index)
-            if dat_index:
-                pairs.append((dat_index, tsf_file_index))
-                self.used_indexes.add(dat_index)
-        return pairs
-
-    def get_pairs(self) -> Tuple[DATFile, TSFile]:
-        for dat_index, tsf_index in self.dat_tsf_pairs:
-            yield self.dat_files[dat_index], self.tsf_files[tsf_index]
-
-
-root = '/media/michael/Data/Projects/GraviSeismicComparation/' \
-       'ZapolarnoeDeposit/2021/src_data'
-
-finder = PairFinder(root)
-print(finder.create_pairs())
