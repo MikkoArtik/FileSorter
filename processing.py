02925b57433474fb1c5fecb8ab3615660c25fdc7
Michael Chernov <mihail.tchernov@yandex.ru>
2021-11-29 17:55:03 +0300

    Добавлена функция получения пересечения времени измерений гравиразведки и микросейсмики

--- processing.py
+++ processing.py
@@ -15,6 +15,26 @@ from dbase import SqliteDbase
 EXPORT_CORRECTIONS_FOLDER_NAME = 'corrections'
 
 
+def get_intersection_time(grav_time: datetime, seis_time: datetime,
+                          edge_type: str) -> datetime:
+    if seis_time == grav_time:
+        return seis_time
+
+    result = seis_time + timedelta(
+        seconds=grav_time.second - seis_time.second)
+    if edge_type == 'left':
+        if seis_time < grav_time:
+            return grav_time
+        if result < seis_time:
+            result += timedelta(minutes=1)
+    else:
+        if seis_time > grav_time:
+            return grav_time
+        if result > seis_time:
+            result += timedelta(minutes=-1)
+    return result
+
+
 def get_correction_value(grav_ampl: float, energy_ratio: float) -> float:
     return round(grav_ampl * energy_ratio ** 0.5, 4)
 
@@ -33,13 +53,28 @@ class Processing:
             raise OSError
 
         self.config = ConfigFile(config_file_path)
-        self.db = SqliteDbase(self.config.export_root)
+        self.dbase = SqliteDbase(self.config.export_root)
         self.logger = logging.getLogger('Processing')
 
     @property
     def export_corrections_folder(self) -> str:
         return os.path.join(
             self.config.export_root, EXPORT_CORRECTIONS_FOLDER_NAME)
+
+    def set_intersection_times(self):
+        self.dbase.clear_time_intersections()
+        grav_seis_pairs = self.dbase.get_grav_seis_times()
+        for grav_id, seis_id, *times in grav_seis_pairs:
+            grav_dt_start, grav_dt_stop = times[:2]
+            seis_dt_start, seis_dt_stop = times[2:]
+
+            left_limit = get_intersection_time(grav_dt_start, seis_dt_start,
+                                               'left')
+            right_limit = get_intersection_time(grav_dt_stop, seis_dt_stop,
+                                                'right')
+            if left_limit < right_limit:
+                self.dbase.add_time_intersection(grav_id, seis_id,
+                                                 left_limit, right_limit)
 
     def get_energies(self, seis_file_path: str, datetime_min: datetime,
                      datetime_max: datetime,
@@ -74,38 +109,39 @@ class Processing:
         return energies
 
     def save_energies(self):
-        records = self.db.get_time_intersections()
+        self.dbase.delete_all_energies()
+        records = self.dbase.get_time_intersections()
         for i, record in enumerate(records):
             pair_id, seis_file_id = record[0], record[2]
             min_datetime, max_datetime = record[3:5]
-            filepath = self.db.get_seis_file_path_by_id(seis_file_id)
+            filepath = self.dbase.get_seis_file_path_by_id(seis_file_id)
             energies = self.get_energies(filepath, min_datetime,
                                          max_datetime)
-            self.db.add_energies(pair_id, energies)
+            self.dbase.add_energies(pair_id, energies)
             self.logger.debug(f'Remain - {len(records) - i - 1} files')
 
     def add_corrections(self):
-        self.db.clear_corrections()
-        for record in self.db.get_pre_correction_data():
+        self.dbase.clear_corrections()
+        for record in self.dbase.get_pre_correction_data():
             minute_id, amplitude, energy_ratio = record
             correction_value = get_correction_value(amplitude, energy_ratio)
-            self.db.add_single_correction(minute_id, correction_value)
+            self.dbase.add_single_correction(minute_id, correction_value)
 
     def get_link_corrections(
             self, chain_id: int, link_id: int, gravimeter_id: int,
             seismometer_id: int) -> List[Tuple[int, int, int, float]]:
-        is_pair_exists = self.db.is_sensor_pair_exists(
+        is_pair_exists = self.dbase.is_sensor_pair_exists(
             chain_id, link_id, gravimeter_id, seismometer_id)
 
-        session_index = self.db.get_session_index(chain_id, link_id)
+        session_index = self.dbase.get_session_index(chain_id, link_id)
 
         corrections_list = []
         if not is_pair_exists:
-            measures_count = self.db.get_measures_count_by_link_id(link_id)
+            measures_count = self.dbase.get_measures_count_by_link_id(link_id)
             for i in range(measures_count):
                 corrections_list.append((session_index, i + 1, 0, 0))
         else:
-            records = self.db.get_post_corrections_by_params(
+            records = self.dbase.get_post_corrections_by_params(
                 chain_id, link_id, gravimeter_id, seismometer_id)
             for cycle_index, correction in records:
                 if correction is None:
@@ -121,11 +157,11 @@ class Processing:
     def get_chain_corrections(
             self, chain_id: int, gravimeter_id: int,
             seismometer_id: int) -> List[Tuple[int, int, int, float]]:
-        if not self.db.is_chain_has_corrections(
+        if not self.dbase.is_chain_has_corrections(
                 chain_id, gravimeter_id, seismometer_id):
             return []
 
-        link_ids = self.db.get_links_by_chain_id(chain_id)
+        link_ids = self.dbase.get_links_by_chain_id(chain_id)
         corrections_list = []
         for link_id in link_ids:
             link_corrections = self.get_link_corrections(
@@ -153,9 +189,9 @@ class Processing:
                 file_ctx.write(line)
 
     def export_corrections(self):
-        chains_ids = self.db.get_all_chain_ids()
+        chains_ids = self.dbase.get_all_chain_ids()
         for chain_id_val in chains_ids:
-            sensor_pairs = self.db.get_device_pairs_by_chain_id(chain_id_val)
+            sensor_pairs = self.dbase.get_device_pairs_by_chain_id(chain_id_val)
             for gravimeter_id, seismometer_id in sensor_pairs:
                 chain_corrections = self.get_chain_corrections(
                     chain_id_val, gravimeter_id, seismometer_id
@@ -164,14 +200,14 @@ class Processing:
                 if not chain_corrections:
                     continue
 
-                chain_datetime = self.db.get_chain_datetime_by_id(
+                chain_datetime = self.dbase.get_chain_datetime_by_id(
                     chain_id_val)
 
                 gravimeter_short_number = \
-                    self.db.get_gravimeter_short_number_by_id(gravimeter_id)
+                    self.dbase.get_gravimeter_short_number_by_id(gravimeter_id)
 
                 seismometer_number = \
-                    self.db.get_seismometer_number_by_id(seismometer_id)
+                    self.dbase.get_seismometer_number_by_id(seismometer_id)
 
                 correction_filename = format_correction_filename(
                     chain_datetime, gravimeter_short_number,
@@ -179,3 +215,8 @@ class Processing:
                 )
 
                 self.save_corrections(correction_filename, chain_corrections)
+
+    def run(self):
+        self.set_intersection_times()
+        self.save_energies()
+        self.add_corrections()
