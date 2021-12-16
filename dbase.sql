737067badd8a9fc88cba72ed383ba84f0e8829d8
Michael <mihail.tchernov@yandex.ru>
2021-12-16 11:24:02 +0300

    Изменено представление подготовки данных к вводу поправок
    Убрано автоматическое вычисление амплитуды

--- dbase.sql
+++ dbase.sql
@@ -191,7 +191,7 @@ GROUP BY se.time_intersection_id;
 CREATE VIEW pre_correction
 AS
 SELECT gsti.id AS time_intersection_id, gmm.id AS grav_measure_id,
-       ROUND(gl.quite_grav_level - gmm.corr_grav, 4) AS grav_amplitude, Rz
+       gl.quite_grav_level, gmm.corr_grav, Rz
 FROM seis_energy se
 JOIN grav_seis_time_intersections gsti ON gsti.id=se.time_intersection_id
 JOIN minimal_energy me ON me.time_intersection_id=se.time_intersection_id
