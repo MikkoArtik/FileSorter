8516c3197a93c885a1e4d3872bcfe33af2b856a4
Michael <mihail.tchernov@yandex.ru>
2022-04-05 16:02:58 +0300

    Реализована потоковая обработка всех наблюдений

--- vibro-stend/main.py
+++ vibro-stend/main.py
@@ -1,5 +1,5 @@
 from config import StreamConfig
-from processing import Processing
+from processing import MainProcessing
 
 
 if __name__ == '__main__':
@@ -7,12 +7,15 @@ if __name__ == '__main__':
            'Vibrostend/statistics.yml'
     main_config = StreamConfig(path)
 
+    main_proc = MainProcessing(main_config)
+    main_proc.run()
     header = ['DateTime', 'Seismometer', 'Gravimeter', 'Freq', 'Amplitude',
               'Velocity', 'Spectrum-Energy(Seis)', 'Amplitude-Energy(Seis)',
-              'Delta-Amplitude(Seis)', 'Gravity']
+              'Delta-Amplitude(Seis)', 'Gravity', 'GravCorr', 'Acceleration']
     with open('statistics0.dat', 'w') as file_ctx:
         file_ctx.write('\t'.join(header) + '\n')
-        for file_path in main_config.config_files:
-            p = Processing(file_path)
-            for record in p.get_statistics():
+        for pair in main_proc.pairs:
+            records = pair.get_join_data()
+            seis_data = pair.seis_data
+            for record in records:
                 file_ctx.write(record.line + '\n')
